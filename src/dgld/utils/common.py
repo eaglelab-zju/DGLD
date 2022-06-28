ae50df76da6b752354c4c715fc51b84e7a6e4a98
GavinYGM <304753882@qq.com>
2022-06-28 15:26:46 +0800

    fix: merge print.py to common

--- src/dgld/utils/common.py
+++ src/dgld/utils/common.py
@@ -504,4 +504,25 @@ def load_ACM(raw_dir=data_path):
 
     return load_mat_data2dgl(data_path=data_file)
 
+def cprint(x, color='green'):
+    from termcolor import colored
+    if color == 'info':
+        color = 'green'
+        x = 'INFO:' + str(x)
+    if color == 'debug':
+        color = 'red'
+        x = 'DEBUG:' + str(x)
+        
+    print(colored(x, color))
 
+def lcprint(*arr, color='green'):
+    x = ':: '.join([str(i) for i in arr])
+    from termcolor import colored
+    if color.lower() == 'info':
+        color = 'green'
+        x = 'INFO:' + str(x)
+    if color.lower() == 'debug':
+        color = 'red'
+        x = 'DEBUG:' + str(x)
+        
+    print(colored(x, color))
