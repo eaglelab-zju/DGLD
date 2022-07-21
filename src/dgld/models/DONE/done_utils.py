fix: network preprocessing formula

src/dgld/models/DONE/done_utils.py
@@ -1,17 +1,99 @@
-from random import sample
-from re import sub
-from statistics import mean
-import torch
-
-import dgl
-
+import shutil
+import sys
 import os
-import wget
-import numpy as np
+sys.path.append('../../')
+
+import argparse
 from tqdm import tqdm
-import csv
 from copy import deepcopy
 
+import torch
+import dgl
+
+
+def get_parse():
+    """
+    get hyperparameter by parser from command line
+
+    Returns
+    -------
+    final_args_dict : dictionary
+        dict of args parser
+    """
+    parser = argparse.ArgumentParser(
+        description='CONAD: Contrastive Attributed Network Anomaly Detection with Data Augmentation')
+
+    parser.add_argument('--dataset', type=str, default='Cora')
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--logdir', type=str, default='tmp')
+    parser.add_argument('--num_epoch', type=int, default=100, help='Training epoch')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--weight_decay', type=float, default=0.)
+    parser.add_argument('--device', type=str, default='cpu')
+    parser.add_argument('--batch_size', type=int, default=0)
+    
+    args = parser.parse_args()
+
+    if os.path.exists(args.logdir):
+        shutil.rmtree(args.logdir)
+
+    if args.lr is None:
+        if args.dataset in ['Cora', 'Citeseer', 'Pubmed', 'Flickr']:
+            args.lr = 1e-3
+        elif args.dataset == 'ACM':
+            args.lr = 5e-4
+        elif args.dataset == 'BlogCatalog':
+            args.lr = 3e-3
+        elif args.dataset == 'ogbn-arxiv':
+            args.lr = 1e-3
+
+    if args.num_epoch is None:
+        if args.dataset in ['Cora', 'Citeseer', 'Pubmed']:
+            args.num_epoch = 100
+        elif args.dataset in ['BlogCatalog', 'Flickr', 'ACM']:
+            args.num_epoch = 400
+        else:
+            args.num_epoch = 10
+            
+    in_feature_map = {
+        "Cora":1433,
+        "Citeseer":3703,
+        "Pubmed":500,
+        "BlogCatalog":8189,
+        "Flickr":12047,
+        "ACM":8337,
+        "ogbn-arxiv":128,
+    }
+    num_nodes_map={
+        "Cora":2708,
+        "Citeseer":3327,
+        "Pubmed":19717,
+        "BlogCatalog":5196,
+        "Flickr":7575,
+        "ACM":16484,
+        "ogbn-arxiv":169343,
+    }
+    final_args_dict = {
+        "dataset": args.dataset,
+        "seed": args.seed,
+        "model":{
+            "feat_size": in_feature_map[args.dataset],
+            "num_nodes": num_nodes_map[args.dataset],
+        },
+        "fit":{
+            "lr": args.lr,
+            "weight_decay": args.weight_decay,
+            "logdir": args.logdir,
+            "num_epoch": args.num_epoch,
+            "device": args.device,
+            "batch_size": args.batch_size,
+        },
+        "predict":{
+            "device": args.device,
+            "batch_size": args.batch_size,
+        }
+    }
+    return final_args_dict
 
 
 def random_walk_with_restart(g:dgl.DGLGraph, k=3, r=0.3, eweight_name='w', eps=0.1):
@@ -50,102 +132,84 @@ def random_walk_with_restart(g:dgl.DGLGraph, k=3, r=0.3, eweight_name='w', eps=0
     mat = inv_degree @ adj
     X = P = P_0
     for i in range(k):
-        P = r * P @ mat + (1 - r) * P
+        P = r * P @ mat + (1 - r) * P_0 # BUG: fix
         X += P 
     X /= k
     
     newg = dgl.graph(X.nonzero(as_tuple=True))
     newg.edata[eweight_name] = X[newg.edges()]
+    newg.ndata['feat'] = g.ndata['feat']
     return newg
 
 
-def read_csv_file_as_numpy(filepath:str):
-    """Read csv file into numpy format
-
-    Parameters
-    ----------
-    filepath : str
-        file path
-    """
-    with open(filepath, 'r') as fp:
-        rd = csv.reader(fp)
-        ret = []
-        for row in tqdm(rd):
-            ret.append([float(r) for r in row])
-    return np.array(ret)
-
-
-def load_paper_dataset(dataset):
-    """Loading the paper dataset
-
-    Parameters
-    ----------
-    dataset : str
-        The dataset used in the experiments of the paper
-    """
-    if dataset == 'cora':
-        adj = read_csv_file_as_numpy('test_data/cora/A_Final_permuted.csv')
-        feat = read_csv_file_as_numpy('test_data/cora/C_Final_permuted.csv')
-        label = read_csv_file_as_numpy('test_data/cora/labels_Final_permuted.csv')
-        indices = read_csv_file_as_numpy('test_data/cora/permutation.csv')
-    else:
-        adj = read_csv_file_as_numpy('test_data/{}/struct.csv'.format(dataset))
-        feat = read_csv_file_as_numpy('test_data/{}/content.csv'.format(dataset))
-        label = read_csv_file_as_numpy('test_data/{}/label.csv'.format(dataset))
-        indices = read_csv_file_as_numpy('test_data/{}/permutation.csv'.format(dataset))
-    print(adj.shape, feat.shape, label.shape)
-    print("#Nodes: {}".format(adj.shape[0]))
-    print("#Edges: {}".format(adj.nonzero()[0].shape[0]))
-    print("#Labels: ", np.unique(label).shape[0])
-    print("#Attributes: {}".format(feat.shape[1]))
+class RWR(dgl.transforms.BaseTransform):
+    def __init__(self, T=3, r=0.3):
+        super(RWR, self).__init__()
+        self.T = T
+        self.r = r
+        
+    def __call__(self, g, eweight_name='w'):
+        newg = deepcopy(g).remove_self_loop()
     
-    graph = dgl.graph(adj.nonzero())
-    graph.ndata['feat'] = torch.FloatTensor(feat)
-    graph.ndata['label'] = torch.IntTensor(label)
+        adj = newg.adj().to_dense()
+        inv_degree = torch.pow(newg.out_degrees().float(), -1)
+        inv_degree = torch.where(torch.isinf(inv_degree), torch.full_like(inv_degree, 0.1), inv_degree)
+        
+        P_0 = torch.eye(newg.number_of_nodes()).float()
+        mat = inv_degree @ adj
+        X = P = P_0
+        for i in range(self.T):
+            P = self.r * P @ mat + (1 - self.r) * P_0 
+            X += P 
+        X /= self.T
+        
+        newg = dgl.graph(X.nonzero(as_tuple=True))
+        newg.edata[eweight_name] = X[newg.edges()]
+        return newg    
     
-    return graph, indices
 
-
-def loss_func(x, x_hat, c, c_hat, h_a, h_s, hom_str, hom_attr, alphas:dict):
+def loss_func(x, x_hat, c, c_hat, h_a, h_s, hom_str, hom_attr, alphas:dict, pretrain=False):
     # closed form update rules
     # Eq.8
     dx_norm = torch.norm(x-x_hat, dim=1)
     numerator = alphas[0] * dx_norm + alphas[1] * hom_str
     os = numerator / torch.sum(numerator)
-    
     # Eq.9
     da_norm = torch.norm(c-c_hat, dim=1)
     numerator = alphas[2] * da_norm + alphas[3] * hom_attr
     oa = numerator / torch.sum(numerator)
-    
     # Eq.10
     dc_norm = torch.norm(h_s-h_a, dim=1)
     oc = dc_norm / torch.sum(dc_norm)
     
-    # Eq.2
-    loss_prox_str = torch.mean(torch.log(torch.pow(os, -1)) * dx_norm)
-    
-    # Eq.3
-    loss_hom_str = torch.mean(torch.log(torch.pow(os, -1)) * hom_str) 
-    
-    # Eq.4
-    loss_prox_attr = torch.mean(torch.log(torch.pow(oa, -1)) * da_norm)
-    
-    # Eq.5
-    loss_hom_attr = torch.mean(torch.log(torch.pow(oa, -1)) * hom_attr)
-    
-    # Eq.6
-    loss_com = torch.mean(torch.log(torch.pow(oc, -1)) * dc_norm) 
-    
+    # using Adam
+    if pretrain is True:
+        loss_prox_str = torch.mean(dx_norm)     # Eq.2
+        loss_hom_str = torch.mean(hom_str)      # Eq.3
+        loss_prox_attr = torch.mean(da_norm)    # Eq.4
+        loss_hom_attr = torch.mean(hom_attr)    # Eq.5
+        loss_com = torch.mean(dc_norm)          # Eq.6
+    else:
+        # Eq.2
+        loss_prox_str = torch.mean(torch.log(torch.pow(os, -1)) * dx_norm)
+        # Eq.3
+        loss_hom_str = torch.mean(torch.log(torch.pow(os, -1)) * hom_str) 
+        # Eq.4
+        loss_prox_attr = torch.mean(torch.log(torch.pow(oa, -1)) * da_norm)
+        # Eq.5
+        loss_hom_attr = torch.mean(torch.log(torch.pow(oa, -1)) * hom_attr)
+        # Eq.6
+        loss_com = torch.mean(torch.log(torch.pow(oc, -1)) * dc_norm) 
+        
     # Eq.7
     loss = alphas[0] * loss_prox_str + alphas[1] * loss_hom_str + alphas[2] * loss_prox_attr + alphas[3] * loss_hom_attr + alphas[4] * loss_com
     
     score = (oa + os + oc) / 3
     return loss, score
-
+  
     
-def train_step(model, optimizer:torch.optim.Optimizer, graph:dgl.DGLGraph, batch_size:int, alphas:dict, newg):
-    sampler = SubgraphSampler()
+def train_step(model, optimizer:torch.optim.Optimizer, graph:dgl.DGLGraph, adj:torch.Tensor,batch_size:int, alphas:dict, num_neighbors:int, pretrain=False):
+    sampler = SubgraphNeighborSampler(num_neighbors)
     dataloader = dgl.dataloading.DataLoader(
         graph, torch.arange(graph.num_nodes()), sampler,
         batch_size=batch_size,
@@ -156,15 +220,14 @@ def train_step(model, optimizer:torch.optim.Optimizer, graph:dgl.DGLGraph, batch
     epoch_loss = 0
     predict_score = torch.zeros(graph.num_nodes())
     
-    adj = newg.adj().to_dense()
-    
+    # adj = graph.remove_self_loop().adj().to_dense()
     
     for sg in dataloader:
         feat = sg.ndata['feat']
         sub_adj = adj[sg.ndata['_ID']]
         
         h_s, x_hat, h_a, c_hat, hom_str, hom_attr = model(sg, sub_adj, feat)
-        loss, score = loss_func(sub_adj, x_hat, feat, c_hat, h_a, h_s, hom_str, hom_attr, alphas)
+        loss, score = loss_func(sub_adj, x_hat, feat, c_hat, h_a, h_s, hom_str, hom_attr, alphas, pretrain=pretrain)
         
         epoch_loss += loss * sg.num_nodes()
         predict_score[sg.ndata['_ID']] = score.cpu().detach()
@@ -173,11 +236,13 @@ def train_step(model, optimizer:torch.optim.Optimizer, graph:dgl.DGLGraph, batch
         loss.backward()
         optimizer.step()
         
+    epoch_loss /= graph.num_nodes()
+        
     return predict_score, epoch_loss
 
 
-def test_step(model, graph, batch_size, alphas):
-    sampler = SubgraphSampler()
+def test_step(model, graph, adj, batch_size, alphas):
+    sampler = SubgraphNeighborSampler()
     dataloader = dgl.dataloading.DataLoader(
         graph, torch.arange(graph.num_nodes()), sampler,
         batch_size=batch_size,
@@ -187,7 +252,7 @@ def test_step(model, graph, batch_size, alphas):
 
     predict_score = torch.zeros(graph.num_nodes())
     
-    adj = graph.adj().to_dense()
+    # adj = graph.adj().to_dense()
     
     for sg in tqdm(dataloader):
         feat = sg.ndata['feat']
@@ -201,16 +266,12 @@ def test_step(model, graph, batch_size, alphas):
     return predict_score.numpy()
 
 
-class SubgraphSampler(dgl.dataloading.Sampler):
-    def __init__(self):
+class SubgraphNeighborSampler(dgl.dataloading.Sampler):
+    def __init__(self, num_neighbors=-1):
         super().__init__()
+        self.num_neighbors = num_neighbors
 
     def sample(self, g, indices):
-        return g.subgraph(indices)
-    
-
-def recall_at_k(truth, score, k):
-    ranking = np.argsort(-score)
-    top_k = ranking[:k]
-    top_k_label = truth[top_k]
-    return top_k_label.sum() / truth.sum()
+        g = g.subgraph(indices)
+        g = dgl.sampling.sample_neighbors(g, g.nodes(), self.num_neighbors)
+        return g

src/dgld/models/DONE/models.py
@@ -6,14 +6,11 @@ import dgl
 import dgl.function as fn
 
 # TODO: add .
-from done_utils import random_walk_with_restart, train_step, test_step
+from done_utils import random_walk_with_restart, train_step, test_step, RWR
 
 # TODO: del
 from sklearn.metrics import roc_auc_score
-import numpy as np
-from done_utils import load_paper_dataset, recall_at_k
-import warnings
-warnings.filterwarnings("ignore")
+
 
 class DONE():
     def __init__(self, 
@@ -22,20 +19,17 @@ class DONE():
                  embedding_dim=32,
                  num_layers=2,
                  activation=nn.LeakyReLU(negative_slope=0.2),
+                 dropout=0.,
                  ):
-        self.model = DONE_Base(feat_size, num_nodes, embedding_dim, num_layers, activation)
+        self.model = DONE_Base(feat_size, num_nodes, embedding_dim, num_layers, activation, dropout)
     
     def fit(self, 
             graph:dgl.DGLGraph,
             lr=1e-3,
-            weight_dacay=0,
+            weight_decay=0,
             num_epoch=1,
-            num_neighbors=2,
-            alpha1=0.2,
-            alpha2=0.2,
-            alpha3=0.2,
-            alpha4=0.2,
-            alpha5=0.2,
+            num_neighbors=-1,
+            alphas=[0.2]*5,
             logdir='tmp',
             batch_size=0,
             device='cpu',
@@ -53,31 +47,38 @@ class DONE():
         if batch_size == 0:
             batch_size = graph.number_of_nodes()
             
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_dacay)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay)
         
         writer = SummaryWriter(log_dir=logdir)
         
-        # newg = random_walk_with_restart(graph, eps=0)
-        newg = graph
+        # newg = random_walk_with_restart(graph)
+        transform = RWR()
+        newg = transform(graph)
+        adj = newg.adj().to_dense()
         
+        # pretrain w/o the outlier scores
         for epoch in range(num_epoch):
-            score, loss = train_step(self.model, optimizer, graph, batch_size, alphas=[alpha1, alpha2, alpha3, alpha4, alpha5], newg=newg)
-            
-            if y_true is not None:
+            score, loss = train_step(self.model, optimizer, graph, adj, batch_size, alphas, num_neighbors, pretrain=True)
+            print(f"Epoch: {epoch:04d}, pretrain/loss={loss:.5f}")
+        
+        # train with the outlier scores
+        for epoch in range(num_epoch):
+            score, loss = train_step(self.model, optimizer, graph, adj, batch_size, alphas, num_neighbors)
+            if y_true is not None and len(y_true.shape)==1:
                 auc = roc_auc_score(y_true, score)
                 print(f"Epoch: {epoch:04d}, train/loss={loss:.5f}, train/auc: {auc:.5f}")
+                writer.add_scalar('train/auc', auc, epoch)
+            else:
+                print(f"Epoch: {epoch:04d}, train/loss={loss:.5f}")
                 
             writer.add_scalar('train/loss', loss, epoch)
-            writer.add_scalar('train/auc', auc, epoch)
+            writer.flush()
     
     def predict(self, 
                 graph:dgl.DGLGraph, 
                 batch_size:int, 
-                alpha1=0.2,
-                alpha2=0.2,
-                alpha3=0.2,
-                alpha4=0.2,
-                alpha5=0.2,
+                device='cpu',
+                alphas=[0.2]*5,
                 ):
         print('*'*20,'predict','*'*20)
         
@@ -88,22 +89,28 @@ class DONE():
             device = torch.device("cpu")
             print('Using cpu!!!') 
             
-        predict_score = test_step(self.model, graph, batch_size, alphas=[alpha1, alpha2, alpha3, alpha4, alpha5])
+        if batch_size == 0:
+            batch_size = graph.number_of_nodes()
+        
+        newg = random_walk_with_restart(graph)
+        adj = newg.adj().to_dense()
+            
+        predict_score = test_step(self.model, graph, adj, batch_size, alphas)
         
         return predict_score
    
     
 class DONE_Base(nn.Module):
-    def __init__(self, feat_size, num_nodes, hid_feats, num_layers, activation):
+    def __init__(self, feat_size, num_nodes, hid_feats, num_layers, activation, dropout):
         super(DONE_Base, self).__init__()
         
-        self.attr_encoder = self._add_mlp(feat_size, hid_feats, hid_feats, num_layers, activation)
+        self.attr_encoder = self._add_mlp(feat_size, hid_feats, hid_feats, num_layers, activation, dropout)
         
-        self.attr_decoder = self._add_mlp(hid_feats, hid_feats, feat_size, num_layers, activation)
+        self.attr_decoder = self._add_mlp(hid_feats, hid_feats, feat_size, num_layers, activation, dropout)
         
-        self.struct_encoder = self._add_mlp(num_nodes, hid_feats, hid_feats, num_layers, activation)
+        self.struct_encoder = self._add_mlp(num_nodes, hid_feats, hid_feats, num_layers, activation, dropout)
         
-        self.struct_decoder = self._add_mlp(hid_feats, hid_feats, num_nodes, num_layers, activation)
+        self.struct_decoder = self._add_mlp(hid_feats, hid_feats, num_nodes, num_layers, activation, dropout)
     
     def forward(self, g, x, c):
         """Forward Propagation
@@ -118,21 +125,22 @@ class DONE_Base(nn.Module):
             attribute matrix
 
         """
-        # structure
-        h_s = self.struct_encoder(x)
-        x_hat = self.struct_decoder(h_s)
-        g.ndata['h'] = h_s
-        g.update_all(self._homophily_loss_message_func, fn.mean('hh', 'h_str'))
-        h_str = g.ndata.pop('h_str')
-        # attribute
-        h_a = self.attr_encoder(c)
-        c_hat = self.attr_decoder(h_a)
-        g.ndata['h'] = h_a
-        g.update_all(self._homophily_loss_message_func, fn.mean('hh', 'h_attr'))
-        h_attr = g.ndata.pop('h_attr')
-        return h_s, x_hat, h_a, c_hat, h_str, h_attr
+        with g.local_scope():
+            # structure
+            h_s = self.struct_encoder(x)
+            x_hat = self.struct_decoder(h_s)
+            g.ndata['h'] = h_s
+            g.update_all(self._homophily_loss_message_func, fn.mean('hh', 'h_str'))
+            
+            # attribute
+            h_a = self.attr_encoder(c)
+            c_hat = self.attr_decoder(h_a)
+            g.ndata['h'] = h_a
+            g.update_all(self._homophily_loss_message_func, fn.mean('hh', 'h_attr'))
+            
+            return h_s, x_hat, h_a, c_hat, g.ndata['h_str'], g.ndata['h_attr']
     
-    def _add_mlp(self, in_feats, hid_feats, out_feats, num_layers, activation):
+    def _add_mlp(self, in_feats, hid_feats, out_feats, num_layers, activation, dropout):
         assert(num_layers >= 2)
         mlp = nn.Sequential()
         # input layer
@@ -140,37 +148,14 @@ class DONE_Base(nn.Module):
         mlp.add_module('act_in', activation)
         # hidden layers
         for i in range(num_layers-2):
+            mlp.add_module(f'dropout_hid_{i}', nn.Dropout(dropout))
             mlp.add_module(f'linear_hid_{i}', nn.Linear(hid_feats, hid_feats))
             mlp.add_module(f'act_hid_{i}', activation)
         # output layer
+        mlp.add_module('dropout_out', nn.Dropout(dropout))
         mlp.add_module('linear_out', nn.Linear(hid_feats, out_feats))
         mlp.add_module('act_out', activation)
         return mlp
     
     def _homophily_loss_message_func(self, edges):
         return {'hh': torch.norm(edges.src['h'] - edges.dst['h'], dim=1)}
-
-
-g, indices = load_paper_dataset('cora')
-# newg = random_walk_with_restart(g)
-feat = g.ndata['feat']
-label = g.ndata.pop('label')
-label_aug = np.zeros_like(label)
-indices = indices.squeeze()
-# print(indices)
-label_aug[indices > 2708] = 1
-# adj = newg.adj().to_dense()
-num_nodes = g.number_of_nodes()
-# model = DONE_Base(feat.shape[1], num_nodes, 32, 2, nn.LeakyReLU(negative_slope=0.2))
-# print(model)
-# h_a, x_hat, h_s, adj_hat, h_str, h_attr = model(g, feat, adj)
-# print(h_a.shape, x_hat.shape, h_s.shape, adj_hat.shape, h_str.shape, h_attr.shape)
-model = DONE(feat.shape[1], num_nodes)
-model.fit(g, batch_size=num_nodes, num_epoch=200, lr=0.03, y_true=label_aug)
-score = model.predict(g, batch_size=num_nodes)
-auc = roc_auc_score(label_aug, score)
-print(f"auc: {auc:.5f}")
-for p in [5, 10, 15, 20, 25]:
-    k = int(num_nodes * p / 100.0)
-    recall = recall_at_k(label_aug, score, k)
-    print(f"Recall@{p}%: {recall:.5f}")